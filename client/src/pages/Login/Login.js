0fd04716387f16e42e87cf08daf52d2ffaebc33d
slacy97 <sheerielacy97@gmail.com>
2020-07-08 11:46:52 -0500

    Dashboardcss

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,69 +1,72 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext } from "react";
 import Message from "../../components/Message/Message";
 import AuthService from "../../Services/AuthService";
 import { AuthContext } from "../../Context/AuthContext";
 // import { Link } from "react-router-dom"
 import "../Login/style.css";
 
+// const styles = {
 
-const Login = props => {
+// }
 
+const Login = (props) => {
   const [user, setUser] = useState({ username: "", password: "" });
   const [message, setMessage] = useState(null);
   const authContext = useContext(AuthContext);
 
-  const onChange = e => {
+  const onChange = (e) => {
     setUser({ ...user, [e.target.name]: e.target.value });
-  }
+  };
 
-  const onSubmit = e => {
+  const onSubmit = (e) => {
     // console.log(data);
     e.preventDefault();
-    AuthService.login(user).then(data => {
+    AuthService.login(user).then((data) => {
       console.log(data);
       const { isAuthenticated, user, message } = data;
       if (isAuthenticated) {
         authContext.setUser(user);
         authContext.setIsAuthenticated(isAuthenticated);
         ///Take to todos page
-        props.history.push('/todos');
-      }
-      else
-        setMessage(message);
+        props.history.push("/todos");
+      } else setMessage(message);
     });
-  }
+  };
 
   return (
-
- 
-
     <div className="container col-lg-12 col-md-12 col-sm-12 col-xs-12">
-    <form className="shadow-lg card " onSubmit={onSubmit}>
-      <div className="card-body">
-        <h3 className="card-title">Welcome back!</h3>
-        <h6>Please login to your account.</h6>
-        <label htmlFor="username" className="user card-text sr-only">Username: </label>
-        <input type="text" 
-               name="username" 
-               onChange={onChange} 
-               className="form-control" 
-               placeholder="Enter Username"/>
-        <label htmlFor="password" className="pass card-text sr-only">Password: </label>
-        <input type="password" 
-               name="password" 
-               onChange={onChange} 
-               className="form-control" 
-               placeholder="Enter Password"/>
-        <button className="btn btn-lg btn-primary btn-block" 
-                type="submit">LOGIN </button>
-
+      <form className="shadow-lg card " onSubmit={onSubmit}>
+        <div className="card-body">
+          <h3 className="card-title">Welcome back!</h3>
+          <h6>Please login to your account.</h6>
+          <label htmlFor="username" className="user card-text sr-only">
+            Username:{" "}
+          </label>
+          <input
+            type="text"
+            name="username"
+            onChange={onChange}
+            className="form-control"
+            placeholder="Enter Username"
+          />
+          <label htmlFor="password" className="pass card-text sr-only">
+            Password:{" "}
+          </label>
+          <input
+            type="password"
+            name="password"
+            onChange={onChange}
+            className="form-control"
+            placeholder="Enter Password"
+          />
+          <button className="btn btn-lg btn-primary btn-block" type="submit">
+            LOGIN{" "}
+          </button>
         </div>
-    </form>
-    {message ? <Message message={message}/> : null}
-</div>
-
-
-  )
-}
+      </form>
+      {message ? <Message message={message} /> : null}
+    </div>
+  );
+};
 
 export default Login;
