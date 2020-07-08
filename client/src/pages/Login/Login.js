08c0c57ec9630e5bd5f9ecdffad068e267772ce9
Aminat.A <aminatadesola@gmail.com>
2020-07-07 19:10:01 -0500

    signup/login working; logout needs to be debugged

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,69 +1,69 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
+import Message from "../../components/Message/Message";
+import AuthService from "../../Services/AuthService";
+import { AuthContext } from "../../Context/AuthContext";
+// import { Link } from "react-router-dom"
 import "../Login/style.css";
-import {Link} from "react-router-dom"
-// import Cards from "../components/Cards";
 
 
-function Login() {
+const Login = props => {
 
-  const [user, setUser] = useState({username : "", password: ""});
-
+  const [user, setUser] = useState({ username: "", password: "" });
+  const [message, setMessage] = useState(null);
+  const authContext = useContext(AuthContext);
 
   const onChange = e => {
-    e.preventDefault();
-   setUser({...user,[e.target.name] : e.target.value})
-   console.log(user)
-  };
+    setUser({ ...user, [e.target.name]: e.target.value });
+  }
 
   const onSubmit = e => {
+    // console.log(data);
     e.preventDefault();
+    AuthService.login(user).then(data => {
+      console.log(data);
+      const { isAuthenticated, user, message } = data;
+      if (isAuthenticated) {
+        authContext.setUser(user);
+        authContext.setIsAuthenticated(isAuthenticated);
+        ///Take to todos page
+        props.history.push('/todos');
+      }
+      else
+        setMessage(message);
+    });
+  }
 
-  };
+  return (
 
-    return(
-      
-      <div class="card ">
-      <div class="card-body">
-        <h5 class="card-title">Login in to InnerCircle</h5>
-        <form onSubmit={onSubmit}>
-          <div class="form-group">
-            <label for="username">Username</label>
-            <input
-            name="username"
-              type="usernameInput"
-              onChange={onChange}
-              class="form-control"
-              id="username"
-              aria-describedby="usernameInput"
-            />
-          </div>
+ 
 
-          <div class="form-group">
-            <label for="passwordInput">Password</label>
-            <input type="password" 
-             name="username"
-             onChange={onChange}
-            class="form-control" 
-            id="passwordInput" />
-            <a href="#" class="forgetPass">
-            
-              Forgot password?
+    <div className="container col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <form className="shadow-lg card " onSubmit={onSubmit}>
+      <div className="card-body">
+        <h3 className="card-title">Welcome back!</h3>
+        <h6>Please login to your account.</h6>
+        <label htmlFor="username" className="user card-text sr-only">Username: </label>
+        <input type="text" 
+               name="username" 
+               onChange={onChange} 
+               className="form-control" 
+               placeholder="Enter Username"/>
+        <label htmlFor="password" className="pass card-text sr-only">Password: </label>
+        <input type="password" 
+               name="password" 
+               onChange={onChange} 
+               className="form-control" 
+               placeholder="Enter Password"/>
+        <button className="btn btn-lg btn-primary btn-block" 
+                type="submit">LOGIN </button>
 
-           </a>
-
-          </div>
-
-          <Link to="/dashboard"><button type="submit" class="btn btn-primary btn-lg btn-block" >
-          LOGIN
-          </button></Link>
-        </form>
-
-
-    </div>
-    </div>
+        </div>
+    </form>
+    {message ? <Message message={message}/> : null}
+</div>
 
 
-    )
+  )
 }
 
 export default Login;
